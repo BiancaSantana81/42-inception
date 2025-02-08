a42a93689fae492c983669ab478aa4ec78476ccb
Bianca Santana <biancasantana81@hotmail.com>
2025-02-08 17:31:54 -0300

    wip -> envs not found in init.sh and databse error

--- srcs/requirements/mariadb/scripts/init.sh
+++ srcs/requirements/mariadb/scripts/init.sh
@@ -1,1 +1,14 @@
-echo "Olá passei por aqui."
+#!/bin/bash
+
+# Executa comandos no banco de dados
+
+#!/bin/bash
+service mariadb start
+
+mysql -u root -e "
+    CREATE DATABASE IF NOT EXISTS ${DATABASE_NAME};
+    CREATE USER IF NOT EXISTS '${DATABASE_USER}'@'%' IDENTIFIED BY '${DATABASE_PASSWORD}';
+    GRANT ALL PRIVILEGES ON ${DATABASE_NAME}.* TO '${DATABASE_USER}'@'%';
+    FLUSH PRIVILEGES;"
+
+echo "Banco de dados e usuário administrador criados com sucesso!"
