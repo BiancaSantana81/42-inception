707d732aa4c96ef6a68d261a2e6a36e7340df787
BiancaSantana81 <biancasantana81@hotmail.com>
2025-02-21 17:48:00 -0300

    inception done: adição da documentação do projeto em inglês

--- srcs/requirements/mariadb/conf/run.sh
+++ srcs/requirements/mariadb/conf/run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+# Start the MariaDB service
 service mariadb start
 
+# Run SQL commands to create the database and the admin user
 mariadb -u root -e \
     "CREATE DATABASE IF NOT EXISTS ${DATABASE_NAME}; \
     CREATE USER IF NOT EXISTS '${ADMIN_NAME}'@'%' IDENTIFIED BY '${ADMIN_PASSWORD}'; \
@@ -10,4 +12,6 @@ mariadb -u root -e \
 
 # Set root password
 mysqladmin -u root password '${ROOT_PASSWORD}'
+
+# Reload the privileges again after setting the root password
 mariadb -u root -e "FLUSH PRIVILEGES;"
--- srcs/requirements/wordpress/conf/run.sh
+++ srcs/requirements/wordpress/conf/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# Configuração do banco de dados
+# WordPress database setup using wp-cli
+# The wp-cli creates the WordPress configuration file with the provided database credentials
 wp --allow-root config create \
     --dbname="$DATABASE_NAME" \
     --dbuser="$ADMIN_NAME" \
@@ -8,7 +9,8 @@ wp --allow-root config create \
     --dbhost=mariadb \
     --dbprefix="wp_"
 
-# Instalação do WordPress
+# WordPress installation
+# The wp-cli command installs WordPress with the provided parameters: site title, URL, and admin credentials
 wp core install --allow-root \
     --path=/var/www/html \
     --title="$WP_TITLE" \
@@ -17,7 +19,8 @@ wp core install --allow-root \
     --admin_password="$ADMIN_PASSWORD" \
     --admin_email="$ADMIN_EMAIL"
 
-# Criação de um usuário adicional
+# Creating an additional user
+# The wp-cli creates a new user with the given username, email, and password
 wp user create --allow-root \
     --path=/var/www/html \
     "$USER_NAME" \
@@ -25,8 +28,8 @@ wp user create --allow-root \
     --user_pass="$USER_PASSWORD" \
     --role='author'
 
-# Ativar o tema
+# Activating the default WordPress theme (Twenty Twenty-Four)
 wp --allow-root theme activate twentytwentyfour
 
-# Garantir que o PHP-FPM está rodando
+# Ensuring PHP-FPM is running to process PHP requests
 exec php-fpm7.4 -F
