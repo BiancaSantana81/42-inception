ab86815ff20bc1c2d658f1582aa9934a510228dc
BiancaSantana81 <biancasantana81@hotmail.com>
2025-02-21 16:05:05 -0300

    update run.sh

--- srcs/requirements/mariadb/conf/run.sh
+++ srcs/requirements/mariadb/conf/run.sh
@@ -7,3 +7,5 @@ mariadb -u root -e \
     CREATE USER IF NOT EXISTS '${ADMIN_NAME}'@'%' IDENTIFIED BY '${ADMIN_PASSWORD}'; \
     GRANT ALL PRIVILEGES ON ${DATABASE_NAME}.* TO '${ADMIN_NAME}'@'%'; \
     FLUSH PRIVILEGES;"
+
+mysqladmin -u root password '${ROOT_PASSWORD}'
